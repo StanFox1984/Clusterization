5a0e0cb12edd828ddae707177bd9c5990e5d10e1
Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
2024-05-28 10:28:16 +0300

    Add heuristical value to each connection and take this into account while choosing optimal route.

--- dejkstra.py
+++ dejkstra.py
@@ -1,33 +1,55 @@
 class Node:
-	def __init__(self, name, value = None):
-		self.name = name
-		self.value = value
-		self.connections = [ ]
+	PrintDebugs = False
 
-	def connect(self, node, connect_back = True):
-		self.connections.append((node, node))
+	def __init__(self, name, data = None):
+		self.name = name
+		self.data = data
+		self.connections = [ ]
+		self.connection_value_map = { }
+
+	def connect(self, node, value = 1, connect_back = True):
+		if not node in self.connections:
+			self.connections.append(node)
+		self.connection_value_map[node.name] = value
 		if connect_back:
-			node.connect(self, False)
+			node.connect(self, value, False)
+
+	def get_connection_value(self, node):
+		if not node.name in self.connection_value_map:
+			return None
+
+		return self.connection_value_map[node.name]
 
 	def __repr__(self):
 		return self.name
 
+	def get_path_heuristic_value(path):
+		value = 0
+		prev_node = None
+		for node in path:
+			if prev_node != None:
+				value += prev_node.get_connection_value(node)
+			prev_node = node
+
+		if Node.PrintDebugs:
+			print ("Heuristic value for path ", path, " is ", value)
+		return value
+
 	def get_shortest_paths(self, paths = { }, path = [ ]):
 		if len(path) == 0:
 			path.append(self)
-		for connection in self.connections:
-			if connection[0] in path:
+		for node in self.connections:
+			if node in path:
 				continue
 			new_path = list(path)
-			new_path.append(connection[0])
-			if not connection[0].name in paths:
-				paths[connection[0].name] = (new_path, len(new_path))
+			new_path.append(node)
+			value = len(new_path) * Node.get_path_heuristic_value(new_path)
+			if not node.name in paths:
+				paths[node.name] = (new_path, value)
 			else:
-				if len(new_path) < paths[connection[0].name][1]:
-					paths[connection[0].name] = (new_path, len(new_path))
-			connection[0].get_shortest_paths(paths, new_path)
-
-
+				if value < paths[node.name][1]:
+					paths[node.name] = (new_path, value)
+			node.get_shortest_paths(paths, new_path)
 
 
 
@@ -38,14 +60,21 @@ d = Node("d")
 e = Node("e")
 
 
-a.connect(b)
-a.connect(c)
-b.connect(c)
-c.connect(d)
-e.connect(d)
-b.connect(e)
+# if value was 1, best route to "e" from "A" would have been A, b, e
+# now it should be a, c, b, e, despite more edges on the way, cost
+# of the connection between a <-> b is too high..
+a.connect(b, 200)
+#a.connect(b, 1)
+a.connect(c, 1)
+b.connect(c, 1)
+c.connect(d, 1)
+e.connect(d, 1)
+b.connect(e, 1)
 
 paths = {}
+
+#Node.PrintDebugs = True
+
 a.get_shortest_paths(paths)
 
 print (paths)
